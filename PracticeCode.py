d2734cf0b4390dc66762753829008bf31d52ea37
SuzzieEm <flashysusanna@yahoo.com>
2025-05-05 14:45:43 -0700

    created a new file and added a few lines of code to it

---
+++ PracticeCode.py
@@ -0,0 +1,7 @@
+name = input("please state your name")
+gender = input("Please state your assigned gender")
+first_adjective = input("How are you feeling today?")
+second_adjective = input("How do you feel about the future?")
+print(f'''{name}, is a, {gender}, who is, {first_adjective}, today, but is , {second_adjective}, about the future. She is determined to keep going until she makes it!''')
+
+             
